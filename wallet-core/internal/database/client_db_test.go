c1cfe3e22f1d1d5d9e09ba5b2ac20ffbbc2c46e0
Tuba <brunogabrielsd@hotmail.com>
2023-08-12 19:47:45 -0300

    feat: added persistence layer

---
+++ wallet-core/internal/database/client_db_test.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/IamP5/ms-wallet/internal/entity"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type ClientDBTestSuite struct {
+	suite.Suite
+	db       *sql.DB
+	clientDB *ClientDB
+}
+
+func (s *ClientDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		panic(err)
+	}
+
+	s.db = db
+	db.Exec("CREATE TABLE clients (id varchar(255), name varchar(255), email varchar(255), created_at date)")
+
+	s.clientDB = NewClientDB(db)
+}
+
+func (s *ClientDBTestSuite) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+}
+
+func TestClientDBTestSuite(t *testing.T) {
+	suite.Run(t, new(ClientDBTestSuite))
+}
+
+func (s *ClientDBTestSuite) TestSave() {
+	client := &entity.Client{
+		ID:    "1",
+		Name:  "John Doe",
+		Email: "j@j",
+	}
+	err := s.clientDB.Save(client)
+	s.Nil(err)
+}
+
+func (s *ClientDBTestSuite) TestFindByID() {
+	client, _ := entity.NewClient("John Doe", "j@j")
+	s.clientDB.Save(client)
+
+	clientDB, err := s.clientDB.FindByID(client.ID)
+	s.Nil(err)
+	s.Equal(client.ID, clientDB.ID)
+	s.Equal(client.Name, clientDB.Name)
+	s.Equal(client.Email, clientDB.Email)
+}
